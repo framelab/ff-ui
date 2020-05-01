bff01001d7f5ce36a3ed25dcc6e1395be488df4d
Ralph Wiedemeier <ralph@framefactory.ch>
2020-05-01 16:45:22 +0200

    updated configuration

--- test/test.ts
+++ test/test.ts
@@ -5,8 +5,21 @@
  * License: MIT
  */
 
+import * as path from "path";
+import * as moduleAlias from "module-alias";
+
+moduleAlias.addAliases({
+    "@ff/ui": path.resolve(__dirname, "../export"),
+    "@ff/core": path.resolve(__dirname, "../../../ff-core/built/export"),
+});
+
+// define vars on node global object (usually done by Webpack)
+global["ENV_DEVELOPMENT"] = false;
+global["ENV_PRODUCTION"] = true;
+global["ENV_VERSION"] = "Test";
+
 ////////////////////////////////////////////////////////////////////////////////
 
 suite("FF UI", function() {
-    // No tests
+    // no tests
 });
